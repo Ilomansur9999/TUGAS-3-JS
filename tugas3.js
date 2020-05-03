91ad7b095d397bd889c78455a0757dfb125ee42a
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 14:49:01 +0700

    TUGAS 3 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas3.js
@@ -0,0 +1,17 @@
+// SEBELUM DIKIRM NILAI
+
+function mintadata(x = 100){
+	console.log(x);
+}
+mintadata();
+
+
+
+// SESUDAH DIKIRIM NILAI
+
+function mintadata(x = 100){
+	console.log(x);
+
+}
+mintadata(50);
+
